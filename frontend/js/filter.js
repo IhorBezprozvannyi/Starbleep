01f55103e4c5b726815f1a6271f44faf4362e2e9
Ihor Bezprozvannyi <bi092006@gmail.com>
2026-05-01 19:15:34 +0200

    filter ready!

--- frontend/js/filter.js
+++ frontend/js/filter.js
@@ -1,15 +1,56 @@
 import {getAllMissions} from "./getRoverInfo.js";
 import {display} from "./representMars.js";
 
+const placedPositions = []; // keep this outside display()
+
+export function getSpacedPosition(x, y, minDistance = 2) {
+  let newX = x;
+  let newY = y;
+  
+  for (const pos of placedPositions) {
+    const dx = newX - pos.x;
+    const dy = newY - pos.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    
+    if (distance < minDistance) {
+      // push away from the conflicting position
+      newX += dx < 0 ? -minDistance : minDistance;
+      newY += dy < 0 ? -minDistance : minDistance;
+    }
+  }
+  
+  placedPositions.push({ x: newX, y: newY });
+  return [newX, newY];
+}
+
+const yearRanges = {
+  "2000-2010": { from: 2000, to: 2010 },
+  "2010-2020": { from: 2010, to: 2020 },
+  "2020-2030": { from: 2020, to: 2030 }
+};
+
 async function filter(year, type, status){
+    placedPositions.length = 0;
+    document.querySelector(".mars").innerHTML = '';
     const data = await getAllMissions();
     console.log(data)
-    data.forEach(element => {
-        if (element.type == type && element.status == status){
-            console.log(element)
-            display(element);
+    
+    const range = yearRanges[year];
+    console.log(range)
+
+    for (const element of data) {
+        const missionYear = element.launch_year
+        console.log(missionYear)
+        const yearMatch = !range || (missionYear >= range.from && missionYear <= range.to);
+        console.log(yearMatch)
+        const typeMatch = type === "Type" || element.type == type;
+        console.log(typeMatch)
+        const statusMatch = status === "Status" || element.status == status;
+        console.log(statusMatch)
+        if (yearMatch && typeMatch && statusMatch){
+            await display(element);
         }
-    });
+    }
 }
 
 document.querySelector("form").addEventListener("submit", (e) => {
--- frontend/js/representMars.js
+++ frontend/js/representMars.js
@@ -1,4 +1,5 @@
 import {getRoverPath, getAllMissions} from "./getRoverInfo.js";
+import {getSpacedPosition} from "./filter.js"
 
 const data = getAllMissions();
 const path = getRoverPath();
@@ -7,7 +8,7 @@ const path = getRoverPath();
 function latLonToPercent(lat, lon) {
   const x = (lon + 180) / 360 * 100;
   const yFull = (90 - lat) / 180 * 100;
-  const y = yFull * 0.20;
+  const y = yFull * 0.10;
   return [ x, y ];
 }
 
@@ -19,9 +20,12 @@ export async function display(element){
     console.log(coords)
     let arr = latLonToPercent(coords.lat, coords.lon);
     console.log(arr);
-    let lon = arr[0];
-    let lat = arr[1];
+    let x = arr[0];
+    let y = arr[1];
+    let [spacedX, spacedY] = getSpacedPosition(x, y, 5);
     document.querySelector(".mars").innerHTML += `
-        <img class="machine" src="../img/place.svg" style="position: absolute; left: ${lon}%; top: ${lat}%; width: 4vw; height: 4vw; transform: rotate(45deg)">
+        <div class="icon-wrapper" data-name="${element.name}" style="position: absolute; left: ${spacedX}%; top: ${spacedY}%; z-index: 1;">
+            <img class="machine" src="../img/place.svg" style="width: 5vw; height: 5vw; filter: invert(90%);">
+        </div>
     `
 }
